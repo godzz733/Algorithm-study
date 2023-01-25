f42c1ead5f1d22d063e27bc9d90260ce67a173ae
godzz733 <109529335+godzz733@users.noreply.github.com>
2023-01-25 14:16:11 +0900

    Update 스택 수열.py

--- 백준/Silver/1874. 스택 수열/스택 수열.py
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -8,23 +8,23 @@ for i in range(t):
     inp_arr.append(a)
 key = 0
 for i in range(t):
-    if stack==[]:
+    if stack==[]: #스택이 비어있을 상황 생각
         c = arr.pop()
         stack.append(c)
         result.append('+')
+        while inp_arr[i]!=stack[-1]: #현재 입력값 까지 stack에 넣음
+            b =arr.pop()
+            stack.append(b)
+            result.append('+') #넣었으면 '+' 추가
+    elif inp_arr[i]!=stack[-1] and inp_arr[i]>stack[-1]: #만약 지금 stack의 마지막 값이 inp_arr의 값보다 작다면 inp_arr 값까지 arr에서 pop해서 집어넣음
         while inp_arr[i]!=stack[-1]:
             b =arr.pop()
             stack.append(b)
             result.append('+')
-    elif inp_arr[i]!=stack[-1] and inp_arr[i]>stack[-1]:
-        while inp_arr[i]!=stack[-1]:
-            b =arr.pop()
-            stack.append(b)
-            result.append('+')
-    elif inp_arr[i]!=stack[-1] and inp_arr[i]<stack[-1]:
+    elif inp_arr[i]!=stack[-1] and inp_arr[i]<stack[-1]: #만약 stack의 마지막 값이 inp_arr보다 크면서 다르다? == 오류 -> key값을 +1해서 안된다는걸 표시
         key +=1
         break
-    if inp_arr[i]==stack[-1]:
+    if inp_arr[i]==stack[-1]: #만약 stack의 마지막 값이 inp_arr와 같다면 pop하고 '-' 
         d = stack.pop()
         result.append('-')
 
