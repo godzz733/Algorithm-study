fa493bfb64a4f4af1103f0a2a39091336f7a3e67
godzz733 <109529335+godzz733@users.noreply.github.com>
2023-01-25 14:44:54 +0900

    [Silver III] Title: 2×n 타일링, Time: 192 ms, Memory: 113248 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -0,0 +1,12 @@
+t = int(input())
+arr = [0] * (t+2)
+arr[1] = 1
+arr[2] = 2
+if t==1:
+    print(1)
+elif t==2:
+    print(2)
+else:
+    for i in range(3,t+1):
+        arr[i] = (arr[i-1] + arr[i-2])%10007
+    print(arr[t])
