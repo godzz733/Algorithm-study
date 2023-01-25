41a242f8572aeeeef86a0d3be10ccb975428a8e7
godzz733 <109529335+godzz733@users.noreply.github.com>
2023-01-25 16:28:41 +0900

    Update 골드바흐의 추측.py

--- 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.py
+++ 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.py
@@ -12,17 +12,17 @@ for k in range(a):
         if arr[k]:
             primes.append(k)
             for k in range(2*k,10000,k):
-                arr[k] = False
-    for i in primes:
-        if i <(t/2):
+                arr[k] = False             #에라토스테네스의 체
+    for i in primes: 
+        if i <(t/2): #만약 t/2보다 작은 소수라면 left리스트에 추가
             left.append(i)
-        elif i==int(t/2):
+        elif i==int(t/2): #만약 t/2과 같은 소수라면 middle리스트에 추가
             middle.append(i)
-        else:
+        else: #만약 t/2보다 큰 소수라면 right리스트에 추가
             right.append(i)
-    if middle!=[]:
+    if middle!=[]: #만약 middle이 있다면 그 합이 차이가 가장 작은거
         print(middle[0],middle[0])
-    else:
+    else: # t/2에서 한칸씩 갈때 그 조합이 소수의 합 조합이라면 그게 정답이다.
         for i in range(int(t/2)):
             if (int(t/2)-i) in left and (int(t/2)+i) in right:
                 print((int(t/2)-i),(int(t/2)+i))
