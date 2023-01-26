1eef68de029640057ea7274fa016d5b749d96bf6
godzz733 <109529335+godzz733@users.noreply.github.com>
2023-01-27 00:17:27 +0900

    Update 풍선 공장.py

--- 백준/Silver/15810. 풍선 공장/풍선 공장.py
+++ 백준/Silver/15810. 풍선 공장/풍선 공장.py
@@ -11,9 +11,9 @@ while start<=end:
     for i in arr:
         result+=mid//i
     
-    if result<b:
+    if result<b: #최소값을 구해야하기때문에 결과값이 작을 때 mid에 1을 추가
         start = mid+1
-    else:
+    else: #최소값을 구할 때 까지 mid-1을 함
         end = mid-1
         num = mid
 
