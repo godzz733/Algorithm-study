4caff53aae0f2875c400b5401a0ebf83dd16852a
godzz733 <109529335+godzz733@users.noreply.github.com>
2023-01-29 16:22:45 +0900

    Update 경로 찾기.py

--- 백준/Silver/11403. 경로 찾기/경로 찾기.py
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.py
@@ -2,13 +2,13 @@ import sys
 input = sys.stdin.readline
 n = int(input().rstrip())
 arr = []
-for i in range(n):
+for i in range(n): #맵 정보
     a = [*map(int, input().rstrip().split())]
     arr.append(a)
 for k in range(n):
     for a in range(n):
         for b in range(n):            
-            if arr[a][k] + arr[k][b] == 2:
-                arr[a][b] = 1
+            if arr[a][k] + arr[k][b] == 2: # a->b로 가는길이 잇는지 전부 체크
+                arr[a][b] = 1 # 있다면 표시
 for i in range(n):
     print(' '.join(map(str , arr[i])))
