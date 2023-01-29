2425fa51e1d742ddef8a31ddc60e20aae7cea02e
godzz733 <109529335+godzz733@users.noreply.github.com>
2023-01-29 22:18:41 +0900

    [Silver I] Title: 절댓값 힙, Time: 264 ms, Memory: 121368 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.py
@@ -0,0 +1,15 @@
+import sys
+import heapq as hep
+input = sys.stdin.readline
+n = int(input())
+arr = []
+
+for _ in range(n):
+    a = int(input())
+    if a!=0:
+        hep.heappush(arr,(abs(a),a))
+    else:
+        if arr==[]:
+            print(0)
+        else:
+            print(hep.heappop(arr)[1])
