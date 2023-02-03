f875f9584a36d8c55831b96c33b96389e3badf8d
godzz733 <109529335+godzz733@users.noreply.github.com>
2023-02-03 09:55:06 +0900

    [Gold V] Title: 토마토, Time: 440 ms, Memory: 191036 KB -BaekjoonHub

---
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -0,0 +1,47 @@
+import sys
+from collections import deque
+
+n, m = map(int, sys.stdin.readline().rstrip().split())
+arr = []
+for _ in range(m):
+    arr.append([*map(int, sys.stdin.readline().rstrip().split())])
+tomato = []
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
+for i in range(m):
+    for j in range(n):
+        if arr[i][j] == 1:
+            tomato.append((i, j))
+
+cnt = 0
+
+
+def bfs():
+    global cnt
+    queue = deque()
+    while tomato:
+        queue.extend(tomato)
+        tomato.clear()
+        while queue:
+            x, y = queue.popleft()
+            for i in range(4):
+                nx = x + dx[i]
+                ny = y + dy[i]
+                if nx < 0 or nx >= m or ny < 0 or ny >= n:
+                    pass
+                else:
+                    if arr[nx][ny] == 0:
+                        arr[nx][ny] = 1
+                        tomato.append((nx, ny))
+        cnt += 1
+    return cnt
+key = 0
+bfs()
+for i in arr:
+    for j in range(n):
+        if i[j] == 0:
+            key = 1
+if key == 0:
+    print(cnt-1)
+else:
+    print(-1)
