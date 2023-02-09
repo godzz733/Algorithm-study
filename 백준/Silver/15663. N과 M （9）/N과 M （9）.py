9ada24878fd7f23c458e7cff4b23042bedd12175
godzz733 <109529335+godzz733@users.noreply.github.com>
2023-02-09 16:00:49 +0900

    [Silver II] Title: N과 M (9), Time: 232 ms, Memory: 123632 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.py
@@ -0,0 +1,28 @@
+n, m = map(int, input().split())
+li = [*map(int, input().split())]
+li.sort()
+arr = []
+for j, i in enumerate(li):
+    arr.append((i, j))
+
+result = set()
+li_arr = []
+idx_arr = []
+
+def back():
+    if len(li_arr) == m:
+        if tuple(li_arr) not in result:
+            result.add(tuple(li_arr))
+            print(*li_arr)
+            return
+        else:
+            return
+    for i, j in arr:
+        if j not in idx_arr:
+            idx_arr.append(j)
+            li_arr.append(i)
+            back()
+            li_arr.pop()
+            idx_arr.pop()
+
+back()
