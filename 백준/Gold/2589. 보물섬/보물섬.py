83868a4e83be23900c04c931612bfc7b215a6f0f
godzz733 <109529335+godzz733@users.noreply.github.com>
2023-02-09 09:45:45 +0900

    [Gold V] Title: 보물섬, Time: 760 ms, Memory: 121160 KB -BaekjoonHub

--- 백준/Gold/2589. 보물섬/보물섬.py
+++ 백준/Gold/2589. 보물섬/보물섬.py
@@ -1,42 +1,38 @@
 from collections import deque
-import copy
 #
 n, m = map(int, input().split())
-li = [list(input()) for _ in range(n)]
-result = []
+arr = [list(input()) for _ in range(n)]
 for i in range(n):
     for j in range(m):
-        if li[i][j] == 'W':
-            li[i][j] = 0
+        if arr[i][j] == 'W':
+            arr[i][j] = 0
         else:
-            li[i][j] = 1
-visited = [[False] * m for _ in range(n)]
+            arr[i][j] = 1
 dx = [1, -1, 0, 0]
 dy = [0, 0, 1, -1]
 
-
-def bfs(x, y):
-    global arr
-    arr = copy.deepcopy(li)
-    visited = [[False] * m for _ in range(n)]
-    queue = deque([(x, y)])
+def bfs(x,y,cnt):
+    if not arr[x][y]:
+        return 0
+    _max = -1
+    queue = deque([(x,y,cnt)])
     visited[x][y] = True
     while queue:
-        x, y = queue.popleft()
+        x, y, cnt = queue.popleft()
+        if _max<cnt:
+            _max = cnt
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            if nx < 0 or nx >= n or ny < 0 or ny >= m:
+            if nx<0 or nx>=n or ny<0 or ny>=m:
                 continue
-            if not visited[nx][ny] and arr[nx][ny]:
-                arr[nx][ny] += arr[x][y]
-                queue.append((nx, ny))
+            if arr[nx][ny] and not visited[nx][ny]:
                 visited[nx][ny] = True
+                queue.append((nx,ny,cnt+1))
+    return _max
+result = 0
 for i in range(n):
-    _min = 0
     for j in range(m):
-        bfs(i,j)
-        for k in range(n):
-            result.append(max(arr[k]))
-print(max(result)-1)
-
+        visited = [[False] * m for _ in range(n)]
+        result = max(result, bfs(i,j,0))
+print(result)
