2fd235f83e0b53f2d5f05c7ec1a6ce5c5541a1d9
godzz733 <109529335+godzz733@users.noreply.github.com>
2023-02-16 17:08:53 +0900

    Update 플로이드 2.py

--- 백준/Gold/11780. 플로이드 2/플로이드 2.py
+++ 백준/Gold/11780. 플로이드 2/플로이드 2.py
@@ -10,19 +10,19 @@ for _ in range(m):
 for i in range(1,n+1):
     arr[i][i] = 0
 def find_path(i,j):
-    if visited[i][j] == 0:
+    if visited[i][j] == 0: # 만약 i->j가 k를 거쳐가지 않았다면 그냥 빈 리스트를 리턴
         return []
-    k = visited[i][j]
-    return find_path(i,k) + [k] + find_path(k,j)
+    k = visited[i][j] # 거쳐간 k를 불러옴
+    return find_path(i,k) + [k] + find_path(k,j) #현재 k를 중앙에 저장하고 다시 i->k 까지 거쳐온 경로, k->j 까지 거쳐온 경로를 양 옆에 붙여줌, 만약 i==k 또는 k==j라면 0이니까 빈리스트를 리털할거임
 
 for k in range(1,n+1):
     for i in range(1,n+1):
         for j in range(1,n+1):
             if arr[i][j] > arr[i][k] + arr[k][j]:
                 arr[i][j] = arr[i][k] + arr[k][j]
-                visited[i][j] = k
+                visited[i][j] = k # 만약 i,j가 k를 거쳐 갓다면 저장
 
-for i in range(1,n+1):
+for i in range(1,n+1): # 그냥 문제의 출력형식
     for j in range(1,n+1):
         print(arr[i][j] if arr[i][j] != INF else 0, end=' ')
     print()
@@ -32,6 +32,6 @@ for i in range(1,n+1):
         if arr[i][j] in [0,INF]:
             print(0)
             continue
-        path = [i] + find_path(i,j) + [j]
+        path = [i] + find_path(i,j) + [j] #find_path함수에서는 i와 j가 아니라 그 사이 거쳐운 경로만 리턴하므로 양옆에 [i] 와 [j]를 붙여주고 find_path를 함
         print(len(path), end=' ')
         print(*path)
