d6e7b88c1515cfd22fa3712e9ad14f95a6c3d432
godzz733 <109529335+godzz733@users.noreply.github.com>
2023-03-03 10:26:59 +0900

    [Silver II] Title: 연산자 끼워넣기 (2), Time: 252 ms, Memory: 118268 KB -BaekjoonHub

---
+++ 백준/Silver/15658. 연산자 끼워넣기 （2）/연산자 끼워넣기 （2）.py
@@ -0,0 +1,36 @@
+n = int(input())
+oper = [0,0,0,0]
+arr = [*map(int,input().split())]
+lst = [*map(int,input().split())]
+for i in range(4):
+    oper[i] += lst[i]
+_min = int(1e10)
+_max = -int(1e10)
+def dfs(cnt,result):
+    global _max, _min
+    if cnt > n-1:
+        if result > _max:
+            _max = result
+        if result < _min:
+            _min = result
+        return
+
+    for i in range(4):
+        if oper[i]:
+            oper[i] -= 1
+            if i == 0:
+                now = result + arr[cnt]
+            elif i == 1:
+                now = result - arr[cnt]
+            elif i == 2:
+                now = result * arr[cnt]
+            else:
+                if result<0:
+                    now = -(-result//arr[cnt])
+                else:
+                    now = result//arr[cnt]
+            dfs(cnt+1,now)
+            oper[i] += 1
+dfs(1,arr[0])
+print(_max)
+print(_min)
