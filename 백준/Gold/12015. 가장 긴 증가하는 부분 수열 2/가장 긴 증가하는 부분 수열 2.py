b352b83c74464d00b2d3d7dadf9502a7ea799dd9
godzz733 <109529335+godzz733@users.noreply.github.com>
2023-04-04 15:59:33 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 608 ms, Memory: 244932 KB -BaekjoonHub

---
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.py
@@ -0,0 +1,26 @@
+def binary(x,st,end,size):
+    res = -1
+    while st<=end:
+        mid = (st+end)//2
+        if dp[mid] < x:
+            st = mid + 1
+        else:
+            res = mid
+            end = mid-1
+    if st==size:
+        return -1
+    else:
+        return res
+lis = 0
+n = int(input())
+arr = list(map(int,input().split()))
+dp = [0] * n
+for i in range(n):
+    idx= binary(arr[i],0,lis,lis+1)
+    if idx == -1:
+        dp[lis] = arr[i]
+        lis += 1
+    else:
+        dp[idx] = arr[i]
+
+print(lis)
