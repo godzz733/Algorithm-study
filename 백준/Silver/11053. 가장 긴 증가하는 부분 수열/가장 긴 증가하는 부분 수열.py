fe2ffdb73b148703e5f84280f07b075a33962239
godzz733 <109529335+godzz733@users.noreply.github.com>
2023-04-10 00:49:48 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 128 ms, Memory: 114328 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
@@ -1,12 +1,40 @@
-import sys
-input = sys.stdin.readline
-n = int(input().rstrip())
-arr = [*map(int,input().rstrip().split())]
-dp = [1 for _ in range(n)]
+def binary(x, start, end, size):
+    res = -1
+    while start <= end:
+        mid = (start + end) // 2
+        if dp[mid] < x:
+            start = mid + 1
+        else:
+            res = mid
+            end = mid - 1
+    if start == size:
+        return -1
+    return res
 
-for i in range(1,n):
-    for j in range(i):
-        if arr[i] > arr[j]:
-            dp[i] = max(dp[i],dp[j]+1)
-print(max(dp))
+
+n = int(input())
+arr = list(map(int, input().split()))
+dp = [-1000000001] * n
+lis = 0
+result_idx = 0
+result = []
+for i in range(n):
+    idx = binary(arr[i], 0, lis, lis + 1)
+    if idx == -1:
+        dp[lis] = arr[i]
+        result_idx = i
+        lis += 1
+    else:
+        dp[idx] = arr[i]
+
+result.append(arr[result_idx])
+last = arr[result_idx]
+idx = lis-1
+for i in range(result_idx-1,-1,-1):
+    if arr[i] < last and arr[i] >= dp[idx-1]:
+        result.append(arr[i])
+        last = arr[i]
+        idx -= 1
+print(lis)
+
 
