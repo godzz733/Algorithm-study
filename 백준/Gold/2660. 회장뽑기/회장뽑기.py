829b008e80ac841cd2d97cfead23950879a8c4d0
godzz733 <109529335+godzz733@users.noreply.github.com>
2023-04-11 12:57:12 +0900

    [Gold V] Title: 회장뽑기, Time: 128 ms, Memory: 114328 KB -BaekjoonHub

--- 백준/Gold/2660. 회장뽑기/회장뽑기.py
+++ 백준/Gold/2660. 회장뽑기/회장뽑기.py
@@ -1,27 +1,38 @@
-n = int(input())
-INF = int(1e9)
-arr = [[INF] * (n) for _ in range(n)]
-result = []
-name = []
+import sys
+input = sys.stdin.readline
+def friends(s):
+    visited = [0] * (V + 1)
+    q = [(s, 0)]
+    visited[s] = 1
+    while q:
+        v, cnt = q.pop(0)
+        for w in graph[v]:
+            if not visited[w]:
+                visited[w] = 1
+                q.append((w, cnt+1))
+    candidates.append((s, cnt))
+
+V = int(input())
+graph = [[] for _ in range(V)]
 while 1:
     a, b = map(int, input().split())
-    if a == b == -1:
+    if a == -1:
         break
-    arr[a-1][b-1] = 1
-    arr[b-1][a-1] = 1
-for i in range(n):
-    for j in range(n):
-        if i==j:
-            arr[i][j] = 0
+    graph[a-1].append(b-1)
+    graph[b-1].append(a-1)
 
-for k in range(n):
-    for a in range(n):
-        for b in range(n):
-            arr[a][b] = min(arr[a][b], arr[a][k]+arr[k][b])
-for i in range(n):
-    result.append(max(arr[i]))
-for i in range(n):
-    if result[i] == min(result):
-        name.append(i+1)
-print(min(result),len(name))
-print(*name)
+candidates = []
+for i in range(0, V):
+    friends(i)
+candidates.sort(key=lambda x: (x[1], x[0]))
+
+final_voted = candidates[0][1]
+final_cnt = 0
+final_candidiates = []
+for i in range(V):
+    if candidates[i][1] == final_voted:
+        final_cnt += 1
+        final_candidiates.append(candidates[i][0]+1)
+
+print(final_voted, final_cnt)
+print(*final_candidiates)
