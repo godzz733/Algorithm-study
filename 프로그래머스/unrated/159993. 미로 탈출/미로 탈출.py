c158ff1a829755123fb12bf312fa3bbf9af567ed
godzz733 <109529335+godzz733@users.noreply.github.com>
2023-06-16 17:40:40 +0900

    [unrated] Title: 미로 탈출, Time: 0.12 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/159993. 미로 탈출/미로 탈출.py
+++ 프로그래머스/unrated/159993. 미로 탈출/미로 탈출.py
@@ -1,45 +1,62 @@
 from collections import deque
-
-def fast_way_bfs(maps, start, end):
-    visited = [[0] * 1000 for _ in range(1000)]
-    drs, dcs = [0, 0, 1, -1], [1, -1, 0, 0]
-    queue = deque()
-    queue.append(start)
-
-    distance = 0
-    while len(queue):
-        r, c, dist = queue.popleft()
-        if not(0 <= r < len(maps)) or not(0 <= c < len(maps[0])):
-            continue
-        if maps[r][c] == 'X' or visited[r][c] == True:
-            continue 
-        if maps[r][c] == maps[end[0]][end[1]]:
-            return dist
-        # print(queue)
-        # print(x, y, maps[x][y], dist)
-
-        visited[r][c] = True
-
-        for dr, dc in zip(drs, dcs):
-            if 0 <= r+dr < len(maps) and 0 <= c+dc < len(maps[0]):
-                if maps[r+dr][c+dc] != 'X' and visited[r+dr][c+dc] == False:
-                    queue.append((r+dr, c+dc, dist+1))
-
-    return -1
-
-
+arr = []
+dx = [-1,1,0,0]
+dy = [0,0,1,-1]
+visited = []
 def solution(maps):
-    start, lever, end = 0, 0, 0
-    for r in range(len(maps)):
-        for c in range(len(maps[0])):
-            if maps[r][c] == 'S':
-                start = r, c, 0
-            if maps[r][c] == 'E':
-                end = r, c, 0
-            if maps[r][c] == 'L':
-                lever = r, c, 0
-
-    first, second = fast_way_bfs(maps, start, lever), fast_way_bfs(maps, lever, end)
-    if -1 in [first, second]:
-        return -1
-    return first + second
+    global arr, visited
+    arr = [[0] * len(maps[0]) for _ in range(len(maps))]
+    visited = [[[False] * len(maps[0]) for _ in range(len(maps))] for _ in range(2)]
+    start = [0,0]
+    # -1 벽 1 탈출 2 레버
+    for i in range(len(maps)):
+        for j in range(len(maps[0])):
+            if maps[i][j] == "X":
+                arr[i][j] = -1
+            elif maps[i][j] == "E":
+                arr[i][j] = 1
+            elif maps[i][j] == "L":
+                arr[i][j] = 2
+            elif maps[i][j] == "S":
+                arr[i][j] = 0
+                start = [i,j]
+    answer = bfs(start[0],start[1],len(maps), len(maps[0]))
+    return answer
+def bfs(x,y,n,m):
+    q = deque()
+    q2 = deque()
+    q.append((x,y,0))
+    visited[0][x][y] = True
+    key = 0
+    ans = -1
+    while q:
+        if key:
+            break
+        x,y,cnt = q.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if nx<0 or nx>=n or ny<0 or ny>=m or visited[0][nx][ny] or arr[nx][ny] == -1:
+                continue
+            if arr[nx][ny] != 2:
+                visited[0][nx][ny] = True
+                q.append((nx,ny,cnt+1))
+            else:
+                visited[1][nx][ny] = True
+                q2.append((nx,ny,cnt+1))
+                key = 1
+                break
+    while q2:
+        x,y,cnt = q2.popleft()
+        print(x,y,cnt)
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if nx<0 or nx>=n or ny<0 or ny>=m or visited[1][nx][ny] or arr[nx][ny] == -1:
+                continue
+            if arr[nx][ny] != 1:
+                visited[1][nx][ny] = True
+                q2.append((nx,ny,cnt+1))
+            else:
+                return cnt+1
+    return ans
