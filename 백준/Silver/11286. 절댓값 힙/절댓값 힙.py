e564a3eee8043fbf10d53a398b1850842bbe0808
godzz733 <109529335+godzz733@users.noreply.github.com>
2023-07-14 15:34:28 +0900

    [Silver I] Title: 절댓값 힙, Time: 144 ms, Memory: 39960 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.py
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.py
@@ -1,6 +1,9 @@
-import heapq as heq, sys
+import heapq as heq
+import sys
+input = sys.stdin.readline
+print = sys.stdout.write
 arr = []
 for _ in range(int(input())):
-    x = int(sys.stdin.readline())
-    if not x: print(heq.heappop(arr)[1]) if arr else print(0)
+    x = int(input())
+    if not x: print(str(heq.heappop(arr)[1]) + "\n") if arr else print('0\n')
     else: heq.heappush(arr, (abs(x), x))
