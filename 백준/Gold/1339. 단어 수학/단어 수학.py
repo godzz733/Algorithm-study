d012091d448e622b6745f7f9a9e8315d6e595bd2
godzz733 <109529335+godzz733@users.noreply.github.com>
2023-08-08 12:44:36 +0900

    [Gold IV] Title: 단어 수학, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/1339. 단어 수학/단어 수학.py
+++ 백준/Gold/1339. 단어 수학/단어 수학.py
@@ -1,17 +1,14 @@
-arr = []
-map = {}
-n = int(input())
-for _ in range(n):
-    tem = input()
+import sys
+input = sys.stdin.readline
+arr, map = [0 for _ in range(26)] ,{}
+for _ in range(int(input())):
+    tem = input().rstrip()
     for j in range(len(tem)):
-        if tem[j] not in map: map[tem[j]] = 0
-        map[tem[j]] += 10**(len(tem)-j-1)
+        arr[ord(tem[j])-ord('A')] += 10 ** (len(tem) - j -1)
 ans = 0
-for key,value in map.items():
-    arr.append([value,key])
-arr.sort(key=lambda x : x[0],reverse=True)
+arr.sort(reverse=True)
 idx = 9
-for x,y in arr:
-    ans += idx * x
+for i in range(10):
+    ans += idx * arr[i]
     idx -= 1
 print(ans)
