1bd56f76df534847bef8cf03838ab91c327539b3
godzz733 <109529335+godzz733@users.noreply.github.com>
2023-08-08 19:57:02 +0900

    [Silver II] Title: 타일링, Time: 136 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Silver/1793. 타일링/타일링.py
+++ 백준/Silver/1793. 타일링/타일링.py
@@ -1,8 +1,5 @@
 arr = [1,1]
-for i in range(2,251):
-    arr.append(arr[i-1] + arr[i-2]*2)
+for i in range(2,251): arr.append(arr[i-1] + arr[i-2]*2)
 while 1:
-    try:
-        print(arr[int(input())])
-    except:
-        break
+    try:print(arr[int(input())])
+    except:break
