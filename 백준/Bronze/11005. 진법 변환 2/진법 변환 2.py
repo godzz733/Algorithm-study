8a7e620f8ffb389b51e528b54e4b6f839beb4fc5
godzz733 <109529335+godzz733@users.noreply.github.com>
2023-08-17 21:57:08 +0900

    [Bronze I] Title: 진법 변환 2, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.py
@@ -0,0 +1,12 @@
+dic = {10:'A',11:'B',12:'C',13:'D',14:'E',15:'F',16:'G',17:'H',18:'I',19:'J',20:'K',21:'L',22:'M',23:'N',24:'O',25:'P',26:'Q',27:'R',28:'S',29:'T',30:'U',31:'V',32:'W',33:'X',34:'Y',35:'Z'}
+a,b = map(int,input().split()) 
+arr = []
+while a != 0:
+    tem = a%b
+    if tem >= 10:
+        arr.append(dic[tem])
+    else:
+        arr.append(str(tem))
+    a = a//b
+arr.reverse()
+print(''.join(arr))
