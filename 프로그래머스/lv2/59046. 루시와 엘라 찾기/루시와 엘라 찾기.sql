1907c12ef63efd7a3842f41ddf1962f4a4c39587
godzz733 <109529335+godzz733@users.noreply.github.com>
2023-09-01 09:20:57 +0900

    [level 2] Title: 루시와 엘라 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
@@ -0,0 +1,3 @@
+-- 코드를 입력하세요
+SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE FROM ANIMAL_INS
+WHERE NAME = 'Lucy' or name = 'Ella' or name = 'Pickle' or name = 'Rogan' or name = 'Sabrina' or name = 'Mitty' ORDER BY ANIMAL_ID
