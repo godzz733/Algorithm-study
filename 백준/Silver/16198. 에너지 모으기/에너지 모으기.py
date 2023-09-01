016bd7e2e785c7c3c40e9524155ef09e1bb2e0f1
godzz733 <109529335+godzz733@users.noreply.github.com>
2023-09-01 17:45:19 +0900

    [Silver I] Title: 에너지 모으기, Time: 104 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/16198. 에너지 모으기/에너지 모으기.py
@@ -0,0 +1,12 @@
+n = int(input())
+arr = list(map(int,input().split()))
+ans = 0
+def back(arr,cnt):
+    global ans
+    if len(arr)<3:
+        ans = max(ans,cnt)
+        return
+    for i in range(1,len(arr)-1):
+        back(arr[:i]+arr[i+1:],cnt+arr[i-1]*arr[i+1])
+back(arr,0)
+print(ans)
