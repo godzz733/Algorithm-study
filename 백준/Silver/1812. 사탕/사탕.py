2e24c1abef65180ddb6527ab2b60997206d08d90
godzz733 <109529335+godzz733@users.noreply.github.com>
2023-09-03 22:04:36 +0900

    [Silver IV] Title: 사탕, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1812. 사탕/사탕.py
+++ 백준/Silver/1812. 사탕/사탕.py
@@ -1,9 +1,12 @@
+import sys
+input = sys.stdin.readline
 n = int(input())
 arr = [int(input()) for _ in range(n)]
 lst = arr[::]
-for i in range(1,n):
-    lst[i] = -lst[i-1] + arr[i]
+for i in range(1,n):lst[i] = -lst[i-1] + arr[i]
 ans = [lst[-1]>>1]
+print(ans[0])
 for i in range(n-1):
-    ans.append(arr[i] - ans[-1])
-for i in ans:print(i)
+    tem = arr[i] - ans[-1]
+    ans.append(tem)
+    print(tem)
