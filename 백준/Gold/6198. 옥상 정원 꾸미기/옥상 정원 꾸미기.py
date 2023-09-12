b60d3665d40696ed1a28c33f60c1b67aeeb469cd
godzz733 <109529335+godzz733@users.noreply.github.com>
2023-09-12 13:55:00 +0900

    [Gold V] Title: 옥상 정원 꾸미기, Time: 200 ms, Memory: 121224 KB -BaekjoonHub

--- 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.py
+++ 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.py
@@ -1,20 +1,10 @@
-from collections import deque
 n = int(input())
 arr = [int(input()) for _ in range(n)]
-stack = deque()
+stack = []
 ans = 0
-for i in range(n-1):
-    if arr[i] > arr[i+1]:
-        stack.append((arr[i],i))
-    else:
-        while stack:
-            tem = stack.pop()
-            if tem[0] <= arr[i+1]:
-                ans += (i-tem[1])
-            else:
-                stack.append(tem)
-                break
-while stack:
-    tem = stack.popleft()
-    ans += (n-1-tem[1])
+for i in arr:
+    while stack and stack[-1] <= i:
+        stack.pop()
+    stack.append(i)
+    ans += len(stack) - 1
 print(ans)
