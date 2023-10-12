0cb63cb1f192c66a7f2dfb5cd141aac46e916ac3
godzz733 <godzz733@naver.com>
2023-10-12 13:04:49 +0900

    [Silver III] Title: 선물, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/1166. 선물/선물.py
+++ 백준/Silver/1166. 선물/선물.py
@@ -3,7 +3,7 @@ a,b,c,d = map(int, input().split())
 ans = 0
 st = 0
 fi = max(b,c,d)
-for _ in range(10000):
+for _ in range(100):
     mid = (st+fi)/2
     if (b//mid) * (c//mid) * (d//mid) >= a:
         ans = max(ans,mid)
