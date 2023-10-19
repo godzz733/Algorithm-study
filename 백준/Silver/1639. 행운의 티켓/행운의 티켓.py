6f87e55a2da68123304a522fe05f301e2722ded5
godzz733 <godzz733@naver.com>
2023-10-19 13:45:37 +0900

    [Silver IV] Title: 행운의 티켓, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1639. 행운의 티켓/행운의 티켓.py
+++ 백준/Silver/1639. 행운의 티켓/행운의 티켓.py
@@ -1,13 +1,10 @@
 import sys;input = sys.stdin.readline
 n = input()
-ans = 0
 for i in range(len(n),0,-1):
     if i % 2: continue
     for j in range(len(n)-i):
         tem = n[j:j+i]
-        # print(tem)
-        # print(tem[:len(tem)//2],tem[len(tem)//2:])
         if sum(map(int,tem[:len(tem)//2])) == sum(map(int,tem[len(tem)//2:])):
-            # print(tem,sum(map(int,tem[:len(tem)//2])),sum(map(int,tem[len(tem)//2:])))
-            ans = max(ans,i)
-print(ans)
+            print(i)
+            sys.exit()
+print(0)
