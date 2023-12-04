f02c9b9ce8acb187a7ea41c6bd34ede4adb94d27
godzz733 <godzz733@naver.com>
2023-12-04 17:30:04 +0900

    [Gold IV] Title: 알파벳, Time: 4896 ms, Memory: 160408 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -1,8 +1,7 @@
 import sys; input = sys.stdin.readline
 n,m = map(int, input().split())
 arr = list(input() for _ in range(n))
-dx = [0,0,1,-1]
-dy = [1,-1,0,0]
+D = ((1, 0), (0, 1), (-1, 0), (0, -1))
 ans = 0
 visited = [False] * 26
 visited[ord(arr[0][0])-65] = True
@@ -10,7 +9,7 @@ def dfs(x,y,cnt):
     global ans
     ans = max(ans,cnt)
     for i in range(4):
-        nx,ny = x+dx[i],y+dy[i]
+        nx,ny = x+D[i][0],y+D[i][1]
         if 0<=nx<n and 0<=ny<m and visited[ord(arr[nx][ny])-65] == False:
             visited[ord(arr[nx][ny])-65] = True
             dfs(nx,ny,cnt+1)
