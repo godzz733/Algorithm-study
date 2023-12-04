3a61a1e59ef2c11288f21986abbec4be54ebd642
godzz733 <godzz733@naver.com>
2023-12-04 21:02:51 +0900

    [Silver III] Title: 수 복원하기, Time: 208 ms, Memory: 184048 KB -BaekjoonHub

--- 백준/Silver/2312. 수 복원하기/수 복원하기.py
+++ 백준/Silver/2312. 수 복원하기/수 복원하기.py
@@ -1,7 +1,16 @@
 import sys;input = sys.stdin.readline
 n = int(input())
+prime = [True] * 100001
+prime[0] = prime[1] = False
+for i in range(2, 100001):
+    if prime[i]:
+        for j in range(i+i, 100001, i):
+            prime[j] = False
 for _ in range(n):
     k = int(input())
+    if prime[k]:
+        print(k, 1)
+        continue
     ori = k
     arr = [0] * 100001
     idx = 2
