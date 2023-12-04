afe0ac2536a25deae0a415603c9ae29151f7c9e4
godzz733 <godzz733@naver.com>
2023-12-04 17:46:06 +0900

    [Gold IV] Title: 알파벳, Time: 160 ms, Memory: 110596 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -3,8 +3,8 @@ n,m = map(int, input().split())
 arr = list(input() for _ in range(n))
 D = ((1, 0), (0, 1), (-1, 0), (0, -1))
 ans = 0
-visited = [[''] * m for _ in range(n)]
-stack = [(0,0,1,arr[0][0])]
+visited = [[0] * m for _ in range(n)]
+stack = [(0,0,1,1<<(ord(arr[0][0])-65))]
 while stack:
     x,y,cnt,check = stack.pop()
     ans = max(ans,cnt)
@@ -13,8 +13,8 @@ while stack:
         exit()
     for i in range(4):
         nx,ny = x+D[i][0],y+D[i][1]
-        if 0<=nx<n and 0<=ny<m and arr[nx][ny] not in check:
-            tem = check + arr[nx][ny]
+        if 0<=nx<n and 0<=ny<m and not check & (1<<(ord(arr[nx][ny])-65)):
+            tem = check | (1<<(ord(arr[nx][ny])-65))
             if tem != visited[nx][ny]:
                 visited[nx][ny] = tem
                 stack.append((nx,ny,cnt+1,tem))
