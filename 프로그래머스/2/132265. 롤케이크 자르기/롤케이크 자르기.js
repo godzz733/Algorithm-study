84b06362bb62ce20662e193e58833af426a1d306
godzz733 <godzz733@naver.com>
2024-01-31 21:36:31 +0900

    [level 2] Title: 롤케이크 자르기, Time: 138.90 ms, Memory: 72.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.js
@@ -0,0 +1,24 @@
+function solution(topping) {
+    var answer = 0;
+    var a = new Map();
+    var b = new Map();
+    topping.map(i => {
+        if (a.get(i)){
+            a.set(i,a.get(i)+1);
+        } else {
+            a.set(i,1);
+        }
+    })
+    topping.map(i => {
+        if (!b.get(i)){
+            b.set(i,1);
+        }
+        if (a.get(i) == 1) {
+            a.delete(i);
+        } else {
+            a.set(i,a.get(i)-1);
+        }
+        if (a.size == b.size) answer++;
+    })
+    return answer;
+}
