41a6e5612e2e5204ae38bb3ba128010798bfe0b1
godzz733 <godzz733@naver.com>
2024-02-12 18:08:34 +0900

    [Gold II] Title: 유클리드 게임, Time: 76 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/4342. 유클리드 게임/유클리드 게임.py
+++ 백준/Gold/4342. 유클리드 게임/유클리드 게임.py
@@ -3,38 +3,12 @@ while 1:
     if a == 0 and b == 0:
         break
     a,b = max(a,b), min(a,b)
-    if a%b == 0:
-        print('A wins')
-        continue
-    arr = []
+    ans = 'A'
     while a%b:
-        arr.append(a//b)
+        if a//b > 1:
+            print(ans,'wins')
+            break
         a,b = b, a%b
-    if len(arr) == 1 and arr[0] == 1:
-        print('B wins')
-        continue
-    now, pre = 0,0
-    if arr[-1] == 1:
-        now = 'B'
-        pre = 'A'
+        ans = 'A' if ans == 'B' else 'B'
     else:
-        now = 'A'
-        pre = 'B'
-    for i in range(len(arr)-2,0,-1):
-        if arr[i] == 1:
-            if pre == 'A':
-                now = 'A'
-                pre = 'B'
-            else:
-                now = 'B'
-                pre = 'A'
-        else:
-            now = 'A'
-            pre = 'B'
-    if now == 'A':
-        if arr[0] == 1:
-            print('B wins')
-        else:
-            print('A wins')
-    else:
-        print('A wins')
+        print(ans,'wins')
