9338016381b2fdf46c199f1f15dd5af9162bd40c
godzz733 <godzz733@naver.com>
2024-02-18 20:49:24 +0900

    [Gold III] Title: 우주 탐사선, Time: 160 ms, Memory: 110432 KB -BaekjoonHub

--- 백준/Gold/17182. 우주 탐사선/우주 탐사선.py
+++ 백준/Gold/17182. 우주 탐사선/우주 탐사선.py
@@ -1,4 +1,5 @@
 import sys; input = sys.stdin.readline
+from itertools import permutations
 n,m = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(n)]
 for i in range(n):
@@ -6,21 +7,15 @@ for i in range(n):
         for k in range(n):
             arr[i][j] = min(arr[i][j], arr[i][k]+arr[k][j])
 
-v = set()
-v.add(m)
 ans = 987654321
-def back(idx,cnt):
-    global ans
-    if len(v) == n:
-        ans = min(ans, cnt)
-        return
-    for i in range(n):
-        if i not in v:
-            v.add(i)
-            cnt += arr[idx][i]
-            if cnt < ans:
-                back(i,cnt)
-            v.remove(i)
-            cnt -= arr[idx][i]
-back(m,0)
+t = [i for i in range(n)]
+t.remove(m)
+for i in permutations(t, n-1):
+    temp = 0
+    temp += arr[m][i[0]]
+    for j in range(n-2):
+        temp += arr[i[j]][i[j+1]]
+        if temp > ans:
+            break
+    ans = min(ans, temp)
 print(ans)
