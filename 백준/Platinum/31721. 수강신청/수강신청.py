6bc1b9050683c175ba878987f5c29d17a0b37a6e
godzz733 <godzz733@naver.com>
2024-04-07 16:30:30 +0900

    [Platinum V] Title: 수강신청, Time: 152 ms, Memory: 32576 KB -BaekjoonHub

--- 백준/Platinum/31721. 수강신청/수강신청.py
+++ 백준/Platinum/31721. 수강신청/수강신청.py
@@ -6,23 +6,22 @@ for _ in range(n):
     a,b = map(int,input().split())
     if a >= b: ans += 1
     else:
-        arr.append((a,b))
+        arr.append(a)
 arr.sort(reverse=True)
-# print(arr)
 while arr:
     ans += 1
     arr.pop()
     tem = m-1
     while arr and tem > 0:
-        a,b = arr.pop()
+        a = arr.pop()
         if tem < a:
             if not arr:
                 ans += 1
                 print(ans)
                 exit()
-            c,d = arr.pop()
-            arr.append((c-tem,d))
-            arr.append((a,b))
+            c = arr.pop()
+            arr.append(c-tem)
+            arr.append(a)
             break
         tem -= a
 print(ans)
