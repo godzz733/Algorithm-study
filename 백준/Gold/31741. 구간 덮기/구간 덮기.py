90e5fd4006ea9b3276872155ca55b194662ac5f4
godzz733 <godzz733@naver.com>
2024-04-09 23:13:07 +0900

    [Gold I] Title: 구간 덮기, Time: 948 ms, Memory: 173688 KB -BaekjoonHub

--- 백준/Gold/31741. 구간 덮기/구간 덮기.py
+++ 백준/Gold/31741. 구간 덮기/구간 덮기.py
@@ -1,7 +1,7 @@
 import sys; input = sys.stdin.readline
 n, l, r = map(int, input().split())
 arr = [[],[],[]]
-ans = 9000000001
+ans = int(1e10)
 for _ in range(n):
     a,b = map(int, input().split())
     if a <= l and b >= r:
@@ -10,29 +10,29 @@ for _ in range(n):
     if b < l or a > r:
         continue
     if a <= l:
-        arr[0].append((a,b))
+        arr[0].append(b)
     elif b >= r:
-        arr[2].append((a,b))
+        arr[2].append(a)
     else:
         arr[1].append((a,b))
 if not arr[0] or not arr[2]:
     print(-1)
     exit()
-arr[0].sort(key = lambda x : x[1])
-arr[2].sort(key= lambda x : x[0])
+arr[0].sort()
+arr[2].sort()
 
-def bisect1(x,y):
+def bisect1(y):
     global ans
     st = 0
     fi = len(arr[2]) - 1
     res = 0
     while st <= fi:
         mid = (st + fi) // 2
-        if arr[2][mid][0] > y:
+        if arr[2][mid]> y:
             fi = mid - 1
         else:
             st = mid + 1
-            res = arr[2][mid][0]
+            res = arr[2][mid]
     if not res:
         return
     ans = min(ans, abs(y - res))
@@ -44,11 +44,11 @@ def bisect2(x,y):
     tem = 0
     while st <= fi:
         mid = (st + fi) // 2
-        if arr[0][mid][1] < x:
+        if arr[0][mid] < x:
             st = mid + 1
         else:
             fi = mid - 1
-            res = arr[0][mid][1]
+            res = arr[0][mid]
     if not res:
         return
     tem += abs(res - x)
@@ -57,19 +57,19 @@ def bisect2(x,y):
     res = 0
     while st <= fi:
         mid = (st + fi) // 2
-        if arr[2][mid][0] > y:
+        if arr[2][mid] > y:
             fi = mid - 1
         else:
             st = mid + 1
-            res = arr[2][mid][0]
+            res = arr[2][mid]
     if not res:
         return
     tem += abs(y - res)
     ans = min(ans, tem)
 
-for x,y in arr[0]:
-    bisect1(x,y)
+for x in arr[0]:
+    bisect1(x)
 for x,y in arr[1]:
     bisect2(x,y)
-ans = -1 if ans == 9000000001 else ans
+ans = -1 if ans == int(1e10) else ans
 print(ans)
