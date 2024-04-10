f2a1963edc8082c6130afd2dd488c0d37186d671
godzz733 <godzz733@naver.com>
2024-04-10 10:20:31 +0900

    [Platinum V] Title: 출근하기 싫어 1, Time: 104 ms, Memory: 108080 KB, Score: 24 point -BaekjoonHub

---
+++ 백준/Platinum/31537. 출근하기 싫어 1/출근하기 싫어 1.py
@@ -0,0 +1,20 @@
+import sys; input = sys.stdin.readline
+n,m = map(int, input().split())
+arr = list(map(int, input().split()))
+for i in range(n):
+    arr[i] = (m - arr[i])
+if sum(arr) > m:
+    print(0)
+    exit()
+ans = 1
+mod = int(1e9) + 7
+for i in arr:
+    for j in range(i):
+        ans *= m
+        ans %= mod
+        m -= 1
+    for j in range(i,0,-1):
+        ans //= j
+    ans %= mod
+
+print(ans % mod)
