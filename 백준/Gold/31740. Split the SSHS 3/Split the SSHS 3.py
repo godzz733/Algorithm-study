13eee7cf698cfa0823cdb1cb2b12d00a47ce914c
godzz733 <godzz733@naver.com>
2024-04-11 19:01:47 +0900

    [Gold IV] Title: Split the SSHS 3, Time: 468 ms, Memory: 73476 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/31740. Split the SSHS 3/Split the SSHS 3.py
+++ 백준/Gold/31740. Split the SSHS 3/Split the SSHS 3.py
@@ -1,4 +1,5 @@
 import sys; input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
 n = int(input())
 arr = [[] for _ in range(n+1)]
 for _ in range(n-1):
@@ -9,6 +10,7 @@ num = [0] + [int(input()) for _ in range(n)]
 v = [0]*(n+1)
 p = [0]*(n+1)   
 def tree(x,pre):
+    if v[x]: return num[x]
     v[x] = 1
     p[x] = pre
     for i in arr[x]:
@@ -16,10 +18,10 @@ def tree(x,pre):
         num[x] += tree(i,x)
     return num[x]
 tree(1,0)
-ans = [987654321,0]
+ans = [int(1e12),0]
 for i in range(2,n+1):
     if abs(num[1]-2*num[i]) < ans[0]:
         ans[0] = abs(num[1] - 2*num[i])
         ans[1] = i
 print(ans[0])
-print(p[ans[1]],ans[1])
+print(min(p[ans[1]],ans[1]),max(p[ans[1]],ans[1]))
