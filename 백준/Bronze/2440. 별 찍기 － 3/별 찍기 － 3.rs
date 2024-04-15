7e8701167fa51299bcab12489a17a5046f2b4856
godzz733 <godzz733@naver.com>
2024-04-15 15:17:37 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 0 ms, Memory: 13212 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.rs
@@ -0,0 +1,28 @@
+use std::{cmp, io, vec};
+use std::io::prelude::*;
+
+fn read<T>(si: &mut T) -> String where T: Read {
+    let mut s = String::new();
+    si.read_to_string(&mut s).unwrap();
+    s
+}
+
+fn next<T>(it: &mut std::str::SplitAsciiWhitespace) -> T where
+    T: std::str::FromStr,
+    <T as std::str::FromStr>::Err: std::fmt::Debug {
+    it.next().unwrap().parse().unwrap()
+}
+
+
+fn main() {
+    // 입력
+    let mut si = io::BufReader::new(io::stdin().lock());
+    let mut so = io::BufWriter::new(io::stdout().lock());
+    let s = read(&mut si);
+    let mut it = s.split_ascii_whitespace();
+
+    let n: i32 = next(&mut it);
+    for i in (1..=n).rev() {
+        writeln!(so,"{}", (0..i).map(|_| "*").collect::<String>()).ok();
+    }
+}
