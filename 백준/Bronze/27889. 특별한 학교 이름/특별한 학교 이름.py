5c2d7142941b28e216016b4ee45a5affd9ac76e8
godzz733 <godzz733@naver.com>
2024-10-14 15:27:40 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 88 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.py
@@ -0,0 +1,3 @@
+import sys; input = sys.stdin.readline
+dic = {"NLCS" : "North London Collegiate School", "BHA" : "Branksome Hall Asia", "KIS" : "Korea International School", "SJA" : "St. Johnsbury Academy"}
+print(dic[input().strip()])
